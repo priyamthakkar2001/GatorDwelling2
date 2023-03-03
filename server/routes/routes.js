d2a12ab18f6e2ef6a64873da2e4276d757f79463
Lauren Newman <laurnewman1@gmail.com>
2023-03-03 00:05:00 -0500

    starting over and crying

---
+++ server/routes/routes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const dbo = require("../db/conn");
+const ObjectId = require("mongodb").ObjectId;
+
+router.route("/dwellings").get(async function (req, res) {
+    try {
+        const data = await dbo.getDb().collection("dwellings").find({}).toArray();
+        res.status(200).send(data);
+    } catch {
+        res.status(400).type('json').send(err);
+    }
+});
+
+router.route("/reviews/:id").get(async function (req, res) {
+    try {
+        const data = await dbo.getDb().collection("reviews").find({id: req.params.id}).toArray();
+        res.status(200).send(data);
+    } catch {
+        res.status(400).type('json').send(err);
+    }
+});
+
+module.exports = router;
