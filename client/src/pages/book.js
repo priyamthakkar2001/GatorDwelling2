6f6bf30976f0dc4ae616ebf4cb647258c26642ea
devanshi25112001 <68034830+devanshi25112001@users.noreply.github.com>
2023-03-08 16:35:15 -0500

    Update book.js

--- client/src/pages/book.js
+++ client/src/pages/book.js
@@ -1,95 +1,158 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Grid, TextField, Typography, Button } from '@material-ui/core';
-import { DatePicker, TimePicker } from '@material-ui/pickers';
+import React, { useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
 
-const useStyles = makeStyles((theme) => ({
-  form: {
-    marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(3),
-  },
-  field: {
-    margin: theme.spacing(1),
-  },
-}));
 
-function ContactForm() {
-  const classes = useStyles();
+
+
+export default function Book() {
+  // Get the ID of the apartment complex from the URL
+  const { id } = useParams();
+
+  // Use the useNavigate hook to redirect the user after form submission
+  const navigate = useNavigate();
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  
+    time: "",
+    message: "",
+  });
+  
+  
+
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Send the form data to the server
+    console.log(formData);
+    // Reset the form data
+    setFormData({
+      name: "",
+      email: "",
+      message: "",
+    });
+    // Navigate to the success page
+    navigate("/success");
+  };
+
+  function BoxSx() {
+    return (
+      <Box
+        sx={{
+          width: 400,
+          backgroundColor: "background.default",
+          boxShadow: 2,
+          p: 2,
+          height: 500,
+
+        }}
+        
+      >
+        <Typography variant="h5" align="center" mb={2}>
+          Contact Us
+        </Typography>
+        <Divider sx={{ mb: 2 }} />
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                required
+                label="Name"
+                name="name"
+                value={formData.name}
+                onChange={handleInputChange}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                required
+                label="Email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                variant="outlined"
+              />
+            </Grid>
+         
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                required
+                label="Phone"
+                name="Phone"
+                value={formData.phone}
+                onChange={handleInputChange}
+                variant="outlined"
+              />
+            </Grid>
+          
+           
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                required
+                label="Time"
+                name="time"
+                type="time"
+                value={formData.time}
+                onChange={handleInputChange}
+                variant="outlined"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                required
+                multiline
+                rows={4}
+                label="Message"
+                name="message"
+                value={formData.message}
+                onChange={handleInputChange}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                fullWidth
+              >
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Box>
+    );
+  }
 
   return (
-    <form className={classes.form}>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
-          <TextField
-            name="name"
-            label="Name"
-            variant="outlined"
-            fullWidth
-            required
-            className={classes.field}
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            name="address"
-            label="Address"
-            variant="outlined"
-            fullWidth
-            required
-            className={classes.field}
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            name="phone"
-            label="Phone"
-            variant="outlined"
-            fullWidth
-            required
-            className={classes.field}
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            name="message"
-            label="Message"
-            variant="outlined"
-            fullWidth
-            required
-            className={classes.field}
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <DatePicker
-            label="Date"
-            inputVariant="outlined"
-            fullWidth
-            required
-            className={classes.field}
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TimePicker
-            label="Time"
-            inputVariant="outlined"
-            fullWidth
-            required
-            className={classes.field}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            className={classes.field}
-          >
-            Submit
-          </Button>
-        </Grid>
-      </Grid>
-    </form>
+    <div>
+      <Typography variant="h4" align="center" mb={4}>
+        Book an Apartment at Complex {id}
+      </Typography>
+      <BoxSx />
+    </div>
   );
 }
 
-export default ContactForm;
