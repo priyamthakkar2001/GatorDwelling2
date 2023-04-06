b701dc3efcbe61cc8e97aae8ee39be4dd22988b9
priyamthakkar <68277650+priyamthakkar@users.noreply.github.com>
2023-04-05 23:41:05 -0400

    Update homeService.js

--- client/src/services/homeService.js
+++ client/src/services/homeService.js
@@ -7,7 +7,7 @@ export default () => {
 
   useEffect(() => {
     const fetchDwellings = async () => {
-        const res = await fetch('http://localhost:5000/dwellings');
+        const res = await fetch('https://react-app-fxy8.onrender.com/dwellings');
         const data = await res.json();
         setDwellings(data);
         setRecovery(data);
